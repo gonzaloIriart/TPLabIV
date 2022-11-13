8d54fad5483cea63355e588c15501be8d6336af8
gonzaloIriart <gonzaloiriart@gmail.com>
2022-11-13 18:55:31 -0300

    update to use pendingPay

--- Views/keeper/pendingReserves.php
+++ Views/keeper/pendingReserves.php
@@ -9,7 +9,7 @@
     <p class="card-text"><?php echo $reserve->getPet()->getSize() ?></p>
     <p class="card-text">Desde: <?php echo $reserve->getEvent()->getStartDate() ?></p>
     <p class="card-text">Hasta: <?php echo $reserve->getEvent()->getEndDate() ?></p>
-    <a href="<?php echo(FRONT_ROOT . "/Keeper/UpdateEventState/" . $reserve->getEvent()->getEventId()."/" ."reserved"); ?>" class="btn btn-primary">Aceptar</a>
+    <a href="<?php echo(FRONT_ROOT . "/Keeper/UpdateEventState/" . $reserve->getEvent()->getEventId()."/" ."pendingPay"); ?>" class="btn btn-primary">Aceptar</a>
     <a href="<?php echo(FRONT_ROOT . "/Keeper/DeleteReserve/" . $reserve->getReserveId() ); ?>" class="btn btn-outline-danger">Rechazar</a>
   </div>
 </div>
